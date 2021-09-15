287774cf6af851e2e3c6cfbfc58ba4593c254cc8
jsprofessional <jshuman@protonmail.com>
2021-09-15 11:46:50 -0500

    Solved 1/7 fun_todo6

--- platform/Algo_concepts/fundamentals2/fun_todo6.js
+++ platform/Algo_concepts/fundamentals2/fun_todo6.js
@@ -3,7 +3,34 @@
 
 // 0. Threes and Fives
 // Create threesFives() that adds values from 100 and 4000000 (inclusive) if that value is evenly divisible by 3 or 5 but not both. Display the final sum in the console.
+
+const threesAndFives = () => {
+    sum = 0;
+    for (var i = 100; i < 4000000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    console.log(sum);
+}
+
+// threesAndFives();
+
+
 // Second: Create betterThreesFives(start, end) that allows you to enter arbitrary start and end values for your range. Think of threesFives() as betterThreesFives(100,4000000).
+
+const betterThreesAndFives = (start, end) => {
+    sum = 0;
+    for (var i = start; i <= end; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    console.log(sum);
+}
+
+// betterThreesAndFives(1,5);
+// betterThreesAndFives(1,50);
 
 
 // 1. Generate Coin Change
@@ -14,8 +41,32 @@
 // quarters: 3
 // dimes: 1
 // nickels: 1
-// pennies: 4copy
+// pennies: 4
 // Second: can you simplify/shorten your code?
+
+const generateCoinChange = (cents) => {
+    let total = cents;
+    let dollars = 0;
+    let quarters = 0;
+    let dimes = 0;
+    let nickels = 0;
+    let pennies = 0;
+    while (total > 100) {
+        total -= 100;
+        dollars += 1;
+        // console.log(total,dollars);
+        console.log(`dollars: ${dollars}, quarters: ${quarters}, dimes: ${dimes}, nickels: ${nickels}, pennies: ${pennies}.`);
+    } 
+    while (total > 25) {
+        total -= 25;
+        quarters += 1;
+        console.log(`dollars: ${dollars}, quarters: ${quarters}, dimes: ${dimes}, nickels: ${nickels}, pennies: ${pennies}.`);
+    }
+}
+
+// generateCoinChange(69);
+generateCoinChange(420);
+
 
 // Third: add half-dollar (50 cents) and dollar (100 cents) coins with 40 additional characters or less.
 
@@ -52,6 +103,3 @@
 
 // 7. Is Prime
 // Return whether a given integer is prime. Prime numbers are only evenly divisible by themselves and 1. Many highly optimized solutions exist, but for now, just create one that is easy to understand and debug.
-
-
-// Being able to write a T-diagram to keep track of your variables while you write out an algorithm by hand is extremely beneficial. You should use a T-diagram for algorithm challenges this chapter.
