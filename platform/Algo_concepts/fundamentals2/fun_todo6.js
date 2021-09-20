daa2d40d8c034da5af1b1e685e826f680b259076
jsprofessional <jshuman@protonmail.com>
2021-09-20 12:01:58 -0500

    started messy math

--- platform/Algo_concepts/fundamentals2/fun_todo6.js
+++ platform/Algo_concepts/fundamentals2/fun_todo6.js
@@ -87,7 +87,7 @@ const generateCoinChange = (cents) => {
     console.log(`---------- FUNCTION END -----------`);
 }
 
-generateCoinChange(69);
+// generateCoinChange(69);
 // generateCoinChange(420);
 // generateCoinChange(9);
 // generateCoinChange(1068);
@@ -100,10 +100,32 @@ generateCoinChange(69);
 // 2. Messy Math Mashup
 
 // Create a function messyMath(num) that will return the following sum: add all integers from 0 up to the given num, except for the following special cases of our count value:
-// If current count (not num) is evenly divisible by 3, don’t add to the sum; skip to the next count;
-// Otherwise, if the current count is evenly divisible by 7, include it twice in sum instead of once;
 // Regardless of the above, if the current count is exactly 1/3 of num, return -1 immediately.
 // For example, if given num is 4, return 7. If given num is 8, return 34. If given num is 15, return -1.
+
+const messyMath = (num) => {
+    console.log(`---------- FUNCTION START ----------`);
+    let count = 0;
+    for (var i = 0; i <= num; i++) {
+        // If count is evenly divisible by 3, don’t add to the sum; skip to the next count;
+        if (count == num/3) {
+            return -1;
+        }
+        else if (count % 3 == 0) {
+            return;
+        // Otherwise, if count is evenly divisible by 7, include it twice in sum instead of once;
+        } else if (count % 7 == 0) {
+            count += i;
+            count += i;
+        }
+        count += i;
+        console.log(`i: ${i}`);
+    }
+    console.log(`---------- FUNCTION END ----------`);
+}
+
+messyMath(4);
+messyMath(7);
 
 
 // 3. Twelve-Bar Blues
